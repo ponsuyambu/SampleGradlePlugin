93e0612dae3cd5e738626695683cdfc4ec417707
ponsuyambu <suyambu.vps@gmail.com>
2017-04-01 13:34:57 +0200

    code clean up

--- sample-groovy-plugin/src/main/groovy/in/ponshere/sample/groovy/plugin/SampleGroovyPlugin.groovy
+++ sample-groovy-plugin/src/main/groovy/in/ponshere/sample/groovy/plugin/SampleGroovyPlugin.groovy
@@ -15,51 +15,5 @@ class SampleGroovyPlugin extends BasePlugin implements Plugin<Project> {
         mAndroidAppConfigBlock.call();
         mDefaultConfigBlock();
         mProductFlavorsConfigBlock.call();
-
-
-
     }
-
-    @Override
-    void apply(Project project) {
-        super.apply(project)
-    }
-//    @Override
-//    void apply(Project project) {
-//        super.apply(project)
-//        project.configure(project) {
-//
-//
-//
-//
-//
-////            project.extensions.configure(AppExtension.class,android)
-////            project.extensions.add("android",android);
-//
-//
-//
-//            n
-//        }
-//    }
-
-
-//    static List<File> getAndroidSources(Project project) {
-//        project.android.sourceSets.inject([]) {
-//            dirs, sourceSet -> dirs + sourceSet.java.srcDirs
-//        }
-//    }
-
-//    static String getResource(Project project, String resourcePath) {
-//        Set<File> files = new HashSet<>()
-//        files += project.buildscript.configurations.classpath.resolve()
-//        files += project.rootProject.buildscript.configurations.classpath.resolve()
-//        File file = files.find { new JarFile(it).getJarEntry(resourcePath) }
-//        if (file == null) {
-//            return null
-//        } else {
-//            JarFile jarFile = new JarFile(file)
-//            JarEntry jarEntry = jarFile.getJarEntry(resourcePath)
-//            return jarFile.getInputStream(jarEntry).text
-//        }
-//    }
 }
